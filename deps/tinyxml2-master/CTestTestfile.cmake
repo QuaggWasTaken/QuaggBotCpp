e8192ada707254d20a4d4d4a8a5d293d60e4aa2a
ffaulkner <forrestfaulkner1@gmail.com>
2019-06-28 23:41:04 -0500

    Add project files.

---
+++ deps/tinyxml2-master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Sarah Faulkner/CPPProjects/QuaggBot/QuaggBot/deps/tinyxml2-master
+# Build directory: C:/Users/Sarah Faulkner/CPPProjects/QuaggBot/QuaggBot/build/deps/tinyxml2-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ deps/xerces-c-3.2.2/doc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Sarah Faulkner/CPPProjects/QuaggBot/QuaggBot/deps/xerces-c-3.2.2/doc
+# Build directory: C:/Users/Sarah Faulkner/CPPProjects/QuaggBot/QuaggBot/build/deps/xerces-c-3.2.2/doc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
